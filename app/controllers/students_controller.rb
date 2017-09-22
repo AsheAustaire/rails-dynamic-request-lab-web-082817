c8850a4f86bdf7420299476d717bdc6c83b019a4
Ashe Austaire <AsheAustaire@gmail.com>
2017-09-21 23:06:09 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,11 @@
 class StudentsController < ApplicationController
   def index
     @students = Student.all
+    render 'index.html'
+  end
+
+  def show
+    @student = Student.find(params[:id])
+    render 'show.html'
   end
 end
